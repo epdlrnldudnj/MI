d5da19937de1f6dd29dd0ed021a0ea70e5e214db
jungeun <kje0023@sookmyung.ac.kr>
2023-11-23 13:00:11 +0900

    오류 해결해봄

--- app/src/main/java/com/example/mi/ui/Calender/CalenderFragment.kt
+++ app/src/main/java/com/example/mi/ui/Calender/CalenderFragment.kt
@@ -8,6 +8,7 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.mi.databinding.FragmentCalenderBinding
+import com.example.mi.ui.Calender.CalenderViewModel
 
 class CalenderFragment : Fragment() {
 
--- app/src/main/java/com/example/mi/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/mi/ui/dashboard/DashboardFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.mi.databinding.FragmentDashboardBinding
+import com.example.mi.databinding.FragmentIslandBinding
 
 class DashboardFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentIslandBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +25,7 @@ class DashboardFragment : Fragment() {
         val dashboardViewModel =
             ViewModelProvider(this).get(DashboardViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentIslandBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         return root
